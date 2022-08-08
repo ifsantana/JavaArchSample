4f5afa11431415d41b86e6bc1d71b8c4c674c5a5
italo.santana <italo.santana@distilled.ie>
2022-08-08 13:38:45 +0100

    fixing ioc logic

--- ifsantana-hexagonal-arch-parent/common/hexagonal-arch-cross-cutting-ioc/src/main/java/com/ifsantana/hexagonal/crosscutting/ioc/HexagonalArchApplicationLauncher.java
+++ ifsantana-hexagonal-arch-parent/common/hexagonal-arch-cross-cutting-ioc/src/main/java/com/ifsantana/hexagonal/crosscutting/ioc/HexagonalArchApplicationLauncher.java
@@ -2,9 +2,12 @@ package com.ifsantana.hexagonal.crosscutting.ioc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
+import org.springframework.hateoas.support.WebStack;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ifsantana.*")
+@EnableHypermediaSupport(type = HypermediaType.HAL, stacks = WebStack.WEBMVC)
 public class HexagonalArchApplicationLauncher {
   public static void main(String[] args) {
     SpringApplication.run(HexagonalArchApplicationLauncher.class, args);
